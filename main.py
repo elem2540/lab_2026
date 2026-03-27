7d4575e3618bc835354981b44637af0df97e0453
eleonora <eleonoramonni04@gmail.com>
2026-03-27 01:29:40 +0100

    base.html - home.html - products.html - styles.css products da fixare

--- main.py
+++ main.py
@@ -1,24 +1,38 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 
 
 app = FastAPI()
-
-@app.get("/")
-def hello_world(
-        q: str,
-        sort: bool = False
-) -> dict[str, str | bool]:
-    return {"q": q, "sort": sort}
-
-@app.get("/{username}")
-def username_webpage(username: str):
-    return f"This is the webpage of user {username}."
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="./templates")
 
 
-@app.get("/{username}/orders/{order_id}")
-def repository_webpage(
-        username: str,
-        order_id: int,
-        sort: bool = False
-):
-    return f"Order {order_id} for user {username}. Sorted: {sort}."
+products = [
+    {"id": 1, "name": "Product 1", "price": 10.99},
+    {"id": 2, "name": "Product 2", "price": 19.99},
+    {"id": 3, "name": "Product 3", "price": 5.99},
+]
+
+
+@app.get("/", response_class=HTMLResponse)
+def home(request: Request):
+    return templates.TemplateResponse(
+        name="home.html",
+        context={
+            "request": request,
+            "text": "Welcome to the store!"
+        }
+    )
+
+
+@app.get("/products", response_class=HTMLResponse)
+def products(request: Request):
+    return templates.TemplateResponse(
+        name="products.html",
+        context={
+            "request": request,
+            "products": products
+        }
+    )
